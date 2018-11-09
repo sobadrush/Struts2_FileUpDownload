fbb63bd5cdcd6f2461bc5500b83b4f8be9aeefa9
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-11-09 20:52:44 +0800

    使用 ajaxSubmit 完成檔案上傳

--- src/main/java/com/ctbc/struts2/controller/FileUploadController.java
+++ src/main/java/com/ctbc/struts2/controller/FileUploadController.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.struts2.convention.annotation.Action;
@@ -13,11 +15,14 @@ import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.json.annotations.JSON;
 import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-@ParentPackage(value = "struts-default")
+
+//@ParentPackage(value = "struts-default")
+@ParentPackage(value = "json-default")
 @Namespace(value = "/FileUploadController")
 @Controller
 @Results({
@@ -33,6 +38,12 @@ public class FileUploadController extends ActionSupport {
 	
 	private String dirtyWord;
 
+	private Map<String, Object> resultMap;
+	
+	{
+		resultMap = new HashMap<String, Object>();
+	}
+	
 	@Action(value = "fileUploadAction", 
 		results = {
 			@Result(name = "success", type = "dispatcher", location = "/WEB-INF/pages/result.jsp")
@@ -61,7 +72,41 @@ public class FileUploadController extends ActionSupport {
 		}
 		return ActionSupport.SUCCESS;
 	}
+	
+	@Action(value = "fileUploadJsonAction", 
+		results = {
+			@Result(name = "success", type = "json")
+		},
+		interceptorRefs = {
+			@InterceptorRef(value = "fileUpload" , 
+					params = { 
+						"allowedTypes","image/bmp,image/jpeg,image/jpg,image/png,image/gif" , 
+						"maximumSize" , "2048000" /*2MB*/
+					}
+			),
+			@InterceptorRef("defaultStack") // 要加這行才取得到 myFilename
+		}
+	)
+	public String fileUploadJsonAction() {
+		System.out.println("================ fileUploadJsonAction() ================");
+		
+		System.out.println(" dirtyWord >>> " + dirtyWord);
+		
+		File target = new File(String.format("%s%s", "D:/" , myAttachmentFileName));
+		try (BufferedOutputStream buff_os = new BufferedOutputStream(new FileOutputStream(target));) {
+			byte[] byteArray = IOUtils.toByteArray(new FileInputStream(myAttachment));
+			IOUtils.write(byteArray, buff_os);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// -------------------------------
+		resultMap.put("rtnMsg", "SUCCEED");
+		// -------------------------------
+		return ActionSupport.SUCCESS;
+	}
 
+	@JSON(serialize = false) // 指定此屬性不被序列化，也就是Struts2不會將此屬性吐回前端
 	public File getMyAttachment() {
 		return myAttachment;
 	}
@@ -86,12 +131,23 @@ public class FileUploadController extends ActionSupport {
 		this.myAttachmentContentType = myAttachmentContentType;
 	}
 
+	@JSON(serialize = false) // 指定此屬性不被序列化，也就是Struts2不會將此屬性吐回前端
 	public String getDirtyWord() {
 		return dirtyWord;
 	}
 
 	public void setDirtyWord(String dirtyWord) {
 		this.dirtyWord = dirtyWord;
+	}
+
+//	@org.apache.struts2.json.annotations.JSON(name = "myJsonData" /*可指定轉成JSON後的key要叫啥，不指定的話就會用resultMap*/)
+	@org.apache.struts2.json.annotations.JSON(serialize = true)
+	public Map<String, Object> getResultMap() {
+		return resultMap;
+	}
+
+	public void setResultMap(Map<String, Object> resultMap) {
+		this.resultMap = resultMap;
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,11 +7,15 @@
   <head>
   	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta charset="UTF-8">
-    <title>Insert title here</title>
+    <title>測試Struts2 檔案上傳/下載</title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
   </head>
-  <body>
+  <body style="padding:0.5cm">
       <h1 style="color:red">index.jsp</h1>
       
+      <h2 style="color:purple">URL : http://localhost:8080/Struts2_FileUpDownload/index.jsp</h2>
      
       <a href='<c:url value="/TestControllerServlet"/>'>Click_Servlet</a>
       
@@ -21,7 +25,7 @@
 		      
       <hr>		      
 		  
-      <form action='<s:url action="fileUploadAction" namespace="/FileUploadController"/>' method="POST" enctype="multipart/form-data">
+      <form action='<s:url action="fileUploadAction" namespace="/FileUploadController"/>' method="POST" enctype="multipart/form-data" style="border: solid 1px black;">
      	   <label for="myFile">使用一般html Tag</label>
      	   <input type="hidden" name="dirtyWord" value="fuckYou!">
      	   <input type="file" name="myAttachment" onchange="console.log( '%c %s' , 'color:yellow' , this.value)">
@@ -30,11 +34,41 @@
 
 	  <p/>
 	
-	  <s:form action="fileUploadAction" namespace="/FileUploadController" method="POST" enctype="multipart/form-data">
+	  <s:form action="fileUploadAction" namespace="/FileUploadController" method="POST" enctype="multipart/form-data" style="border: solid 1px black;">
 		  <s:file name="myAttachment" label="請選擇檔案(使用Struts2 Tag)" size="1"/>
 		  <s:hidden name="dirtyWord" value="幹你娘"/>
 		  <s:submit value="上傳囉!!!" align="center" />
 	  </s:form>
-		      
+      
+	  <p/>      
+
+	  <form id="myForm" action='javascript:void(0)' method="POST" enctype="multipart/form-data" style="border: solid 1px black;">
+     	   <label for="myFile">使用ajaxSubmit插件：</label>
+     	   <input type="hidden" name="dirtyWord" value="幹你娘">
+     	   <input type="file" name="myAttachment" onchange="console.log( '%c %s' , 'color:yellow' , this.value)"><br>
+     	   <!-- <input type="submit" value="上傳囉"/> -->
+     	   <button id="myBtn" type="button" class="btn btn-primary">-- 上傳囉 --</button>
+      </form>    
+	  
+	  <script>
+	  	$(function(){
+	  		$("#myBtn").click(function(event){
+	  			event.preventDefault();
+	  			$('#myForm').ajaxSubmit({
+	  				url : '<s:url action="fileUploadJsonAction" namespace="/FileUploadController"/>',
+	  	            type : 'post',
+	  	            dataType : 'json', // 預期服務器返回的資料類型，若指定json，則無需再解析json字符串
+	  	            data : {},
+	  	            success : function(data) {
+	  	            	console.log( ' data >>> ' , data );
+	  	                alert(data.resultMap.rtnMsg);
+	  	            }
+	  			});
+	  		});
+	  	});
+	  </script>
   </body>
 </html>
+
+
+
