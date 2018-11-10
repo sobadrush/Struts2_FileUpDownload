5e60922cb73152df1d4e067eec1d9d066110620a
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-11-10 22:31:47 +0800

    使用params root指定要被JSON序列化的屬性

--- src/main/java/com/ctbc/struts2/controller/TestStruts2Controller.java
+++ src/main/java/com/ctbc/struts2/controller/TestStruts2Controller.java
@@ -1,6 +1,10 @@
 package com.ctbc.struts2.controller;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -14,7 +18,8 @@ import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-@ParentPackage(value = "struts-default")
+//@ParentPackage(value = "struts-default")
+@ParentPackage(value = "json-default")
 @Namespace(value = "/TestStruts2Controller")
 @Controller
 @Results({
@@ -26,6 +31,8 @@ public class TestStruts2Controller extends ActionSupport {
 
 	@Autowired
 	private DataSource ds;
+
+	private Map<String, Object> resultFuckMap;
 	
 	@Action(value = "myTest", results = { 
 		@Result(name = "success" , type = "dispatcher" , location = "/WEB-INF/pages/result.jsp" ) 
@@ -41,5 +48,33 @@ public class TestStruts2Controller extends ActionSupport {
 		
 		return ActionSupport.SUCCESS;
 	}
+	
+	// URL : http://localhost:8080/Struts2_FileUpDownload/TestStruts2Controller/myTestJson.action
+	@Action(value = "myTestJson", results = { 
+			@Result(name = SUCCESS , type = "json" , params = { "root" , "resultFuckMap" } ) 
+	})
+	public String myTestJson() {
+		System.out.println("================ myTestJson() ================");
+		List<String> listA = new ArrayList<>();
+		listA.add("華農兄弟");
+		listA.add("這樣不行唷");
+		listA.add("烤竹鼠");
+		
+		resultFuckMap = new HashMap<>();
+		resultFuckMap.put("listA", listA);
+		return SUCCESS;
+	}
+
+	public Map<String, Object> getResultFuckMap() {
+		return resultFuckMap;
+	}
+
+	public void setResultFuckMap(Map<String, Object> resultFuckMap) {
+		this.resultFuckMap = resultFuckMap;
+	}
 
 }
+
+
+
+
